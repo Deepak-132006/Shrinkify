77140661d7f5f9f99263fc32ba026b7746f7df15
Deepak-132006 <deepakn132006@gmail.com>
2026-01-30 09:30:12 +0530

    Initial: Shrinkify from start.spring.io

---
+++ src/main/java/com/deepak/shrinkify/ShrinkifyApplication.java
@@ -0,0 +1,13 @@
+package com.deepak.shrinkify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrinkifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShrinkifyApplication.class, args);
+	}
+
+}
