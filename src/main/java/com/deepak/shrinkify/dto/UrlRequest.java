00f5433e0e973ce6a2b85bd4dcaef81a4ddd3adc
Deepak-132006 <deepakn132006@gmail.com>
2026-01-31 12:40:18 +0530

    Input Validation & Security - Done

--- src/main/java/com/deepak/shrinkify/dto/UrlRequest.java
+++ src/main/java/com/deepak/shrinkify/dto/UrlRequest.java
@@ -2,16 +2,16 @@ package com.deepak.shrinkify.dto;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.validator.constraints.URL;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class UrlRequest {
-    @NotBlank
-    @URL
+    @NotBlank(message = "Original URL cannot be blank")
+    @Size(max = 2000, message = "URL too long")
     private String originalUrl;
 }
